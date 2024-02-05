3f3ba713eaefbfb49f209d27113607869e31fc16
Justin Looi <justinlooijw@gmail.com>
2024-02-05 15:49:14 +0800

    Add android app folder and android generated files

---
+++ next-pwa-android-app/app/build/generated/source/buildConfig/debug/app/vercel/next_basic_pwa/twa/BuildConfig.java
@@ -0,0 +1,12 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package app.vercel.next_basic_pwa.twa;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "app.vercel.next_basic_pwa.twa";
+  public static final String BUILD_TYPE = "debug";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "1";
+}
---
+++ next-pwa-android-app/app/src/main/java/app/vercel/next_basic_pwa/twa/DelegationService.java
@@ -0,0 +1,14 @@
+package app.vercel.next_basic_pwa.twa;
+
+
+
+public class DelegationService extends
+        com.google.androidbrowserhelper.trusted.DelegationService {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        
+    }
+}
+
